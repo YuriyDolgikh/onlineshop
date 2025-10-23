fb3520c64a97f51f4d546c07efd94beb68f54451
“Yuriy <dolgih.yv@gmail.com>
2025-10-23 10:50:19 +0200

    add UserService + DTO

---
+++ src/main/java/org/onlineshop/dto/user/UserUpdateRequestDto.java
@@ -0,0 +1,37 @@
+package org.onlineshop.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserUpdateRequestDto {
+
+    @NotBlank(message = "Email is required and must be not blank)")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    @NotNull
+    @NotBlank(message = "Username is required and must be not blank)")
+    @Size(min = 3, max = 15, message = "Username must be between 3 and 15 characters")
+    private String username;
+
+    @NotNull
+    @NotBlank(message = "Phone number is required and must be not blank)")
+    @Size(min = 6, max = 20, message = "Phone number must be between 6 and 20 characters")
+    private String phoneNumber;
+
+    @NotNull
+    @NotBlank(message = "Password is required and must be not blank)")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    private String hashPassword;
+
+}
