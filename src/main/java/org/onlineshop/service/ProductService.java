18e83f6b06d821dc96f73ea4a017ff4a10c6ac4d
“Yuriy <dolgih.yv@gmail.com>
2025-10-27 22:10:29 +0100

    add ProductService, ProductConverter

--- src/main/java/org/onlineshop/service/ProductService.java
+++ src/main/java/org/onlineshop/service/ProductService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -186,5 +187,10 @@ public class ProductService implements ProductServiceInterface {
             return Sort.Direction.DESC;
         }
         throw new IllegalArgumentException("Invalid sort direction: " + sortDirection);
+    }
+
+    @Override
+    public Optional<Product> getProductById(Integer productId) {
+        return productRepository.findById(productId);
     }
 }
