bbbb99ed58bf5dfe918d0d08ae4a69b2235e3db8
“Yuriy <dolgih.yv@gmail.com>
2025-10-30 11:44:05 +0100

    fix converters

--- src/main/java/org/onlineshop/dto/product/ProductRequestDto.java
+++ src/main/java/org/onlineshop/dto/product/ProductRequestDto.java
@@ -1,8 +1,6 @@
 package org.onlineshop.dto.product;
 
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
@@ -13,18 +11,14 @@ import java.math.BigDecimal;
 @Builder
 public class        ProductRequestDto {
 
-    @NotBlank(message = "product title is required and must be not blank)")
     @Size(min = 3, max = 20, message = "Product title must be between 3 and 20 characters")
     private String productName;
 
-    @NotBlank(message = "product description is required and must be not blank)")
     @Size(min = 3, max = 100, message = "Product description must be between 3 and 100 characters")
     private String productDescription;
 
-    @NotBlank(message = "Category name must be provided")
     private String productCategory;
 
-    @NotNull(message = "Price must be provided")
     @DecimalMin(value = "0.01", message = "Price must be greater than 0")
     private BigDecimal productPrice;
 
