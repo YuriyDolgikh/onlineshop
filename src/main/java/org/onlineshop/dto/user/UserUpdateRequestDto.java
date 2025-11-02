e7fae91b808a337a2a96a7067f6218c32e50fb76
SergeyGrechikhin <blackblock456@gmail.com>
2025-11-02 17:21:02 +0100

    Change UserUpdateRequestDto

--- src/main/java/org/onlineshop/dto/user/UserUpdateRequestDto.java
+++ src/main/java/org/onlineshop/dto/user/UserUpdateRequestDto.java
@@ -15,22 +15,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserUpdateRequestDto {
 
-    @NotNull
-    @NotBlank(message = "Username is required and must be not blank)")
-    @Size(min = 3, max = 15, message = "Username must be between 3 and 15 characters")
     private String username;
 
-    @NotNull
-    @NotBlank(message = "Phone number is required and must be not blank)")
-    @Pattern(
-            regexp = "^\\+?[0-9]{7,15}$",
-            message = "Phone number must contain only digits and may start with +, length 7–15"
-    )
     private String phoneNumber;
 
-    @NotNull
-    @NotBlank(message = "Password is required and must be not blank)")
-    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String hashPassword;
 
 }
