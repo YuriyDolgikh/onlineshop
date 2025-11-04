5191fdddfb0fbfbd437f1ad7fcbfa5b32cff6c95
SergeyGrechikhin <blackblock456@gmail.com>
2025-11-04 13:14:20 +0100

    AddProduct Test .

--- src/test/java/org/onlineshop/service/ProductServiceAddProductTest.java
+++ src/test/java/org/onlineshop/service/ProductServiceAddProductTest.java
@@ -1,11 +1,14 @@
 package org.onlineshop.service;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.onlineshop.dto.product.ProductRequestDto;
 import org.onlineshop.dto.product.ProductResponseDto;
 import org.onlineshop.entity.Category;
+import org.onlineshop.exception.BadRequestException;
 import org.onlineshop.repository.CategoryRepository;
 import org.onlineshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +20,9 @@ import org.springframework.test.context.TestPropertySource;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,6 +38,9 @@ class ProductServiceAddProductTest {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ValidatorFactory validatorFactory;
+
 
     @AfterEach
     void dropDatabase() {
@@ -71,6 +78,71 @@ class ProductServiceAddProductTest {
         assertEquals(savedProduct.getProductName(), requestDto.getProductName());
         assertEquals(savedProduct.getProductCategory(), requestDto.getProductCategory());
         assertEquals(1, productRepository.findAll().size());
+
+    }
+
+    @Test
+    void testAddProductIfCategoryNotFound() {
+
+        ProductRequestDto requestDto = ProductRequestDto.builder()
+                .productName("TestProduct")
+                .productCategory("notTestCategory")
+                .image("https://drive.google.com/test")
+                .productDescription("TestProductText")
+                .productPrice(BigDecimal.valueOf(100))
+                .productDiscountPrice(BigDecimal.valueOf(5))
+                .build();
+
+        Exception exception = assertThrows(BadRequestException.class, () -> productService.addProduct(requestDto));
+        assertEquals("Category with name: " + requestDto.getProductCategory() + " not found", exception.getMessage());
+
+    }
+
+    @Test
+    void testAddProductWhenCategoryAlreadyContainsProductWithSameName() {
+        ProductRequestDto productFirst = ProductRequestDto.builder()
+                .productName("TestProduct")
+                .productCategory("testCategory")
+                .image("https://drive.google.com/test")
+                .productDescription("TestProductText")
+                .productPrice(BigDecimal.valueOf(100))
+                .productDiscountPrice(BigDecimal.valueOf(5))
+                .build();
+
+        productService.addProduct(productFirst);
+
+        ProductRequestDto productSecond = ProductRequestDto.builder()
+                .productName("TestProduct")
+                .productCategory("testCategory")
+                .image("https://drive.google.com/Dublicate")
+                .productDescription("TestProductTextForDublicate")
+                .productPrice(BigDecimal.valueOf(80))
+                .productDiscountPrice(BigDecimal.valueOf(10))
+                .build();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> productService.addProduct(productSecond));
+        String messageException = "Product with name: " + productSecond.getProductName() + " already exist in category: " + productSecond.getProductCategory();
+        assertEquals(messageException, exception.getMessage());
+
+    }
+
+    @Test
+    void testAddProductIfPriceIsZero(){
+        ProductRequestDto product = ProductRequestDto.builder()
+                .productName("TestProduct")
+                .productCategory("testCategory")
+                .image("https://drive.google.com/test")
+                .productDescription("TestProductText")
+                .productPrice(BigDecimal.ZERO)
+                .productDiscountPrice(BigDecimal.valueOf(5))
+                .build();
+
+        Set<ConstraintViolation<ProductRequestDto>> violations = validatorFactory.getValidator().validate(product);
+        assertFalse(violations.isEmpty(), "Validation should fail for price = 0");
+        assertTrue(
+                violations.stream().anyMatch(v -> v.getMessage().equals("Price must be greater than 0")),
+                "Error message should be 'Price must be greater than 0'"
+        );
 
     }
 }
