e82b5bf71b389e6af0da7f447a7bd85ee49123e6
“Yuriy <dolgih.yv@gmail.com>
2025-11-08 12:23:34 +0100

    add Docker properties

--- src/main/java/org/onlineshop/OnlineshopApplication.java
+++ src/main/java/org/onlineshop/OnlineshopApplication.java
@@ -2,10 +2,14 @@ package org.onlineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "org.onlineshop.repository")
+@EntityScan(basePackages = "org.onlineshop.entity")
 public class OnlineshopApplication {
 
     public static void main(String[] args) {
