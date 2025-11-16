28370eabdab25563c6b543a5a372d8d453e0d68b
“Yuriy <dolgih.yv@gmail.com>
2025-11-16 22:19:53 +0100

    fix PdfOrderGenerator

--- src/main/java/org/onlineshop/service/util/PdfOrderGenerator.java
+++ src/main/java/org/onlineshop/service/util/PdfOrderGenerator.java
@@ -12,91 +12,106 @@ import org.springframework.stereotype.Service;
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.format.DateTimeFormatter;
 
 @Service
 public class PdfOrderGenerator {
     public static byte[] generatePdfOrder(Order order) {
-
         try {
-        Document document = new Document();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Document document = new Document();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        PdfWriter.getInstance(document, out);
-        document.open();
+            PdfWriter.getInstance(document, out);
+            document.open();
 
-        Font titleFont = new Font(Font.HELVETICA, 18, Font.BOLD);
-        Paragraph title = new Paragraph("Order Confirmation", titleFont);
-        document.add(title);
-        document.add(Chunk.NEWLINE);
+            Font titleFont = new Font(Font.HELVETICA, 18, Font.BOLD);
+            Paragraph title = new Paragraph("Order Confirmation", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            document.add(title);
+            document.add(Chunk.NEWLINE);
 
-        Font intoFont = new Font(Font.HELVETICA, 12);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
-        document.add(new Paragraph("Order ID: " + order.getOrderId(), intoFont));
-        document.add(new Paragraph("Date: " + order.getCreatedAt().format(formatter), intoFont));
-        document.add(new Paragraph("Customer: " + order.getUser().getUsername(), intoFont));
-        document.add(new Paragraph("Status: " + order.getStatus(), intoFont));
-        document.add(new Paragraph("Delivery method: " + order.getDeliveryMethod(), intoFont));
-        document.add(new Paragraph("Delivery address: " + order.getDeliveryAddress(), intoFont));
-        document.add(new Paragraph("Contact phone:" + order.getContactPhone(), intoFont));
-        document.add(new Paragraph("Email:" + order.getUser().getEmail(), intoFont));
-        document.add(Chunk.NEWLINE);
+            Font infoFont = new Font(Font.HELVETICA, 12);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            document.add(new Paragraph("Order ID: " + order.getOrderId(), infoFont));
+            document.add(new Paragraph("Date: " + order.getCreatedAt().format(formatter), infoFont));
+            document.add(new Paragraph("Customer: " + order.getUser().getUsername(), infoFont));
+            document.add(new Paragraph("Status: " + order.getStatus(), infoFont));
+            document.add(new Paragraph("Delivery method: " + order.getDeliveryMethod(), infoFont));
+            document.add(new Paragraph("Delivery address: " +
+                    (order.getDeliveryAddress() != null ? order.getDeliveryAddress() : "Not specified"), infoFont));
+            document.add(new Paragraph("Contact phone: " +
+                    (order.getContactPhone() != null ? order.getContactPhone() : "Not specified"), infoFont));
+            document.add(new Paragraph("Email: " + order.getUser().getEmail(), infoFont));
+            document.add(Chunk.NEWLINE);
 
-        PdfPTable table = new PdfPTable(5);
-        table.setWidthPercentage(100);
-        table.setSpacingBefore(10f);
-        table.setSpacingAfter(10f);
+            PdfPTable table = new PdfPTable(6);
+            table.setWidthPercentage(100);
+            table.setSpacingBefore(10f);
+            table.setSpacingAfter(10f);
 
-        addTableHelper(table, "Product", "Quantity", "Price for 1 item", "Discount", "Final price", "Total");
-        BigDecimal totalSumm = BigDecimal.ZERO;
-        for (OrderItem item : order.getOrderItems()) {
-            String product = item.getProduct().getName();
-            Integer quantity = item.getQuantity();
-            BigDecimal price = item.getProduct().getPrice();
-            BigDecimal discount = item.getPriceAtPurchase();
-            BigDecimal finalPrice = item.getPriceAtPurchase() != null ? item.getPriceAtPurchase() : item.getProduct().getPrice();
-            BigDecimal total = finalPrice.multiply(BigDecimal.valueOf(quantity));
-            totalSumm = totalSumm.add(total);
+            addTableHeader(table, "Product", "Quantity", "Price for 1 item", "Discount", "Final price", "Total");
 
-            addTableRow(table,
-                    product,
-                    String.valueOf(quantity),
-                    price.toString(),
-                    discount.toString(),
-                    finalPrice.toString(),
-                    total.toString()
-            );
-        }
-        document.add(table);
+            DecimalFormat df = new DecimalFormat("#0.00");
 
-        Font boldFont = new Font(Font.HELVETICA, 13, Font.BOLD);
-        Paragraph totalParagraph = new Paragraph("Pended payment  : " + totalSumm + " €", boldFont);
-        totalParagraph.setAlignment(Element.ALIGN_RIGHT);
-        document.add(totalParagraph);
+            BigDecimal totalSumm = BigDecimal.ZERO;
+            for (OrderItem item : order.getOrderItems()) {
+                String product = item.getProduct().getName();
+                Integer quantity = item.getQuantity();
+                BigDecimal price = item.getProduct().getPrice();
+                BigDecimal discount = item.getProduct().getDiscountPrice();
 
-        document.close();
-        return out.toByteArray();
-    }catch(Exception e){
-            throw  new RuntimeException("Error generating PDF order : " + e.getMessage(),e);
+                BigDecimal finalPrice;
+                if (discount != null && discount.compareTo(BigDecimal.ZERO) > 0) {
+                    finalPrice = price.subtract(price.multiply(discount).divide(BigDecimal.valueOf(100)));
+                } else {
+                    finalPrice = price;
+                }
+
+                BigDecimal total = finalPrice.multiply(BigDecimal.valueOf(quantity));
+                totalSumm = totalSumm.add(total);
+
+                addTableRow(table,
+                        product,
+                        String.valueOf(quantity),
+                        df.format(price),
+                        (discount != null ? df.format(discount) : "0.00") + "%",
+                        df.format(finalPrice),
+                        df.format(total)
+                );
+            }
+            document.add(table);
+            document.add(Chunk.NEWLINE);
+
+            Font boldFont = new Font(Font.HELVETICA, 13, Font.BOLD);
+            Paragraph totalParagraph = new Paragraph("Total amount: " + df.format(totalSumm), boldFont);
+            totalParagraph.setAlignment(Element.ALIGN_RIGHT);
+            document.add(totalParagraph);
+
+            document.close();
+            return out.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating PDF order : " + e.getMessage(), e);
         }
     }
 
-    private static void addTableHelper(PdfPTable table, String... headers) {
+    private static void addTableHeader(PdfPTable table, String... headers) {
         Font headFont = new Font(Font.HELVETICA, 12, Font.BOLD);
-        for (String h : headers) {
-            PdfPCell header = new PdfPCell(new Phrase(h, headFont));
-            header.setHorizontalAlignment(Element.ALIGN_CENTER);
-            header.setBackgroundColor(Color.LIGHT_GRAY);
-            table.addCell(header);
+        for (String header : headers) {
+            PdfPCell cell = new PdfPCell(new Phrase(header, headFont));
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setBackgroundColor(Color.LIGHT_GRAY);
+            cell.setPadding(5);
+            table.addCell(cell);
         }
     }
 
     private static void addTableRow(PdfPTable table, String... values) {
-        for (String v : values) {
-            PdfPCell cell = new PdfPCell(new Phrase(v));
+        for (String value : values) {
+            PdfPCell cell = new PdfPCell(new Phrase(value));
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setPadding(5);
             table.addCell(cell);
         }
-
     }
 }
