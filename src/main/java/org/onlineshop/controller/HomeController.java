4f74be78fa74bae222c0853a4fae883736ae00e4
“Yuriy <dolgih.yv@gmail.com>
2025-11-25 14:49:36 +0100

    add @Valid in controllers, change request methods types in OrderController

--- src/main/java/org/onlineshop/controller/HomeController.java
+++ src/main/java/org/onlineshop/controller/HomeController.java
@@ -3,7 +3,6 @@ package org.onlineshop.controller;
 import lombok.Generated;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @Generated
@@ -12,10 +11,5 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "forward:/index.html";
-    }
-
-    @GetMapping("/modal/{name}")
-    public String getModal(@PathVariable String name) {
-        return "modal/" + name;
     }
 }
