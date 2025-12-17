92a9fb377211748b857ff889e9462995849e0874
“Yuriy <dolgih.yv@gmail.com>
2025-12-17 11:56:43 +0100

    fix OrderServiceTests

--- src/main/java/org/onlineshop/dto/favourite/FavouriteResponseDto.java
+++ src/main/java/org/onlineshop/dto/favourite/FavouriteResponseDto.java
@@ -1,13 +1,11 @@
 package org.onlineshop.dto.favourite;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Objects;
 
 @Data
+@Generated
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
